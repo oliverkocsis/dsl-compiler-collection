92617a13af89482ea82b72427b55f7ac986cbaa9
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-03-01 19:57:44 +0100

    Move expected result into a dedicated git repo

--- src/back-end/angular.templates.ts
+++ src/back-end/angular.templates.ts
@@ -2,7 +2,8 @@ export const COMPONENT_CLASS = `import { Component, OnInit } from '@angular/core
 
 @Component({
   selector: 'app-{{kebab}}',
-  templateUrl: './{{kebab}}.component.html'
+  templateUrl: './{{kebab}}.component.html',
+  styleUrls: ['./{{kebab}}.component.css']
 })
 export class {{camel}}Component implements OnInit {
 
