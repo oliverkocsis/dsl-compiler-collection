2613b6a70bea9f8617b6806e6f80a8af19bcc1d5
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-03-21 22:54:32 +0100

    BugFix: fix import on index.ts, add PropertyNode

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-export { AbstractSyntaxGraph } from './abstract-syntax-graph/abstract-syntax-graph';
-export { DataNode } from './abstract-syntax-graph/data-node';
+export { AbstractSyntaxGraph, DataNode, PropertyNode } from './abstract-syntax-graph/abstract-syntax-graph';
 export { Backend } from './backend/backend';
 export { Project } from './project/project';
 export { Directory } from './project/directory';
