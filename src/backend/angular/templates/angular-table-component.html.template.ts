72d8c15aa7dce5e5f3d22247cfce3b2b05e7fd62
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-03-29 13:39:41 +0200

    TDD

---
+++ src/backend/angular/templates/angular-table-component.html.template.ts
@@ -0,0 +1,21 @@
+export const ANGULAR_COMPONENT_HTML_TEMPLATE = `<form [formGroup]="formGroup" novalidate (ngSubmit)="onSubmit()">
+<mat-card class="shipping-card">
+  <mat-card-header>
+    <mat-card-title>{{name}}</mat-card-title>
+  </mat-card-header>
+  <mat-card-content>
+    {{#properties}}
+    <div class="row">
+      <div class="col">
+        <mat-form-field class="full-width">
+          <input matInput placeholder="{{name}}" type="{{htmlType}}" formControlName="{{camel}}">
+        </mat-form-field>
+      </div>
+    </div>
+    {{/properties}}
+  </mat-card-content>
+  <mat-card-actions>
+    <button mat-flat-button color="primary" type="submit">Submit</button>
+  </mat-card-actions>
+</mat-card>
+</form>`
---
+++ src/backend/angular/templates/angular-table-component.ts.template.ts
@@ -0,0 +1,27 @@
+export const ANGULAR_COMPONENT_TS_TEMPLATE = `import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { {{pascal}}Service } from '../{{kebab}}.service';
+import { {{pascal}} } from '../{{kebab}}';
+
+@Component({
+  selector: 'app-{{kebab}}-form',
+  templateUrl: './{{kebab}}-form.component.html',
+  styleUrls: ['./{{kebab}}-form.component.scss']
+})
+export class {{pascal}}FormComponent {
+  formGroup = this.fb.group({
+    {{#properties}}
+    {{camel}}: null,
+    {{/properties}}
+  });
+
+  constructor(private fb: FormBuilder, private service: {{pascal}}Service) { }
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+    const {{camel}} = {{pascal}}.from(this.formGroup.value);
+    this.service.create({{camel}});
+    this.formGroup.reset();
+  }
+}
+`
