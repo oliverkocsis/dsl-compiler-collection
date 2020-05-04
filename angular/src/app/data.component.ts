f12f100646bbd6173a1e80397cc1e3e46775d19c
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-05-04 22:17:41 +0200

    redirect trial

--- angular/src/app/data.component.ts
+++ angular/src/app/data.component.ts
@@ -1,5 +1,5 @@
 import { ViewChild, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormComponent } from './data-form.component';
 import { Data } from './data';
@@ -7,15 +7,23 @@ import { Data } from './data';
 export abstract class DataComponent<D extends Data, F extends FormComponent<D>> implements OnInit {
 
   id: string;
-  redirect: string[];
+  current: string;
+  redirect: string;
+  reference: string;
   @ViewChild('formComponent') formComponent: F;
 
   constructor(protected router: Router, protected route: ActivatedRoute, protected location: Location) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      console.log(`Router.params: ${JSON.stringify(params)}`);
+      console.log(`router.params: ${JSON.stringify(params)}`);
       this.id = params.id;
+      this.reference = params.reference;
+      this.redirect = params.redirect;
+    });
+    this.route.url.subscribe((urls: UrlSegment[]) => {
+      this.current = urls.join('/');
+      console.log(`url: ${this.current}`);
     });
   }
 
@@ -31,7 +39,11 @@ export abstract class DataComponent<D extends Data, F extends FormComponent<D>> 
 
   saveClose() {
     this.save();
-    this.location.back();
+    if (this.redirect) {
+      this.router.navigate([this.redirect, { reference: this.id }]);
+    } else {
+      this.location.back();
+    }
   }
 
 }
