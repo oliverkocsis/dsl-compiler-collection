75dc39a622616cab42b31d157cb31b219b4e5fbc
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-05-04 17:49:32 +0200

    clean up data classes

--- angular/src/app/account/account.ts
+++ angular/src/app/account/account.ts
@@ -5,22 +5,4 @@ export class Account extends Data {
   public phone: string;
   public website: string;
   public address: string;
-
-  constructor() { super() }
-
-  from(data: Account) {
-    this._id = data._id;
-    this.name = data.name;
-    this.phone = data.phone;
-    this.website = data.website;
-    this.address = data.address;
-    return this;
-  }
-
-  static from(data: Account) {
-    const _this = new Account();
-    _this.from(data);
-    return _this;
-  }
-
 }
