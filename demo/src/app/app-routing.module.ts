c6b07194f1570378d2baafe5f90d9cb9bedd832e
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-05-23 21:18:26 +0200

    Generate material form

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyFormComponent } from './my-form/my-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'my-form', component: MyFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyFormComponent } from './my-form/my-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
